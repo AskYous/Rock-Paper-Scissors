4e7a63bb3cd77b9eb6405588e6f5bf35f6179f91
Carlos Toledo <kqarlos@hotmail.com>
2012-04-20 21:28:48 -0700

    fixes to randomcalc

---
+++ JavaApplication4/src/RockPaperScissors/RandomCalculator.java
@@ -0,0 +1,35 @@
+package RockPaperScissors;
+
+import java.util.Random;
+
+/**
+ *
+ * @author Yousef_2
+ */
+public class RandomCalculator extends CalculateWeapon{
+
+    public RandomCalculator() {
+    }
+
+    /**
+     * Randomly chooses a weapon
+     * @return a weapon
+     */
+    @Override
+    public Weapon calculateWeapon() {
+        Weapon w = null;
+        Random rnd = new Random();
+        int code = rnd.nextInt(4);
+        while (code == 0) {
+            code = rnd.nextInt(3);
+        }
+        if (code == 1) {
+            w = Weapon.ROCK;
+        } else if (code == 2) {
+            w = Weapon.PAPER;
+        } else if (code == 3) {
+            w = Weapon.SCISSORS;
+        }
+        return w;
+    }
+}
